4d991ee8b2c10f3fca3b9c3d8aca11647944ce7a
Saee Patil <saeenppatil@gmail.com>
2026-02-08 17:14:39 -0800

    added more table functions and fixed bugs

--- lstore/table.py
+++ lstore/table.py
@@ -45,8 +45,6 @@ class Table:
 
         self.new_pages(is_tail = False)
         self.new_pages(is_tail = True)
-  
-        pass
 
     # New pages and capacity
 
@@ -63,11 +61,11 @@ class Table:
         else:
             return self.base_pages[-1][0].has_capacity()
 
-    def tail_capacity(self, range_id: int):
+    def tail_capacity(self):
         if not self.tail_pages:
             return False
         else:
-            return self.tail_pages[-1][0].has_capacity() # -----------!! I think the -1 here should be range_id !! ------------
+            return self.tail_pages[-1][0].has_capacity() 
 
 
     # RID helpers 
@@ -91,7 +89,7 @@ class Table:
             pages = self.tail_pages
         else: 
             if not self.base_capacity():
-                self.new_pages(is_tail = True) # -------!! should be False !!----------
+                self.new_pages(is_tail = False) 
             pages = self.base_pages
         
         pages_id = len(pages) - 1
@@ -103,9 +101,8 @@ class Table:
             if not temp: 
                 raise RuntimeError("page has no capacity")
 
-        # -----------!! need to update page directory !!-------------
-        # rid = values[RID_COLUMN]
-        # self.page_directory[rid] = (is_tail, pages_id, index)
+        rid = values[RID_COLUMN]
+        self.page_directory[rid] = (is_tail, pages_id, index)
 
         return pages_id, index
 
@@ -127,16 +124,102 @@ class Table:
         cols = [self.read_val(rid, 4 + c) for c in range(self.num_columns)]
         return cols
 
+    def get_schemaenc(self, rid: int):
+        return self.read_val(rid, SCHEMA_ENCODING_COLUMN)
+
     # more records functions 
 
     def insert_base_record(self, cols: list[int]):
-        pass
+        
+        if len(cols) != self.num_columns:
+            raise ValueError("wrong no. of columns")
+
+        base_rid = self.get_RID()
+
+        curr_time = int(time())
+
+        values = [0] * self.total_columns
+        values[INDIRECTION_COLUMN] = INVALID_RID
+        values[RID_COLUMN] = base_rid
+        values[TIMESTAMP_COLUMN] = curr_time
+        values[SCHEMA_ENCODING_COLUMN] = 0
+        for i, x in enumerate(cols):
+            values[4 + i] = int(x)
+        
+        pages_id, index = self.write_record(is_tail = False, values = values)
+
+        self.base_indirection[base_rid] = INVALID_RID
+        self.base_schema[base_rid] = 0  #not sure how to handle this 
+        
+        key_val = cols[self.key]
+        self.index.add_entry(self.key, base_rid, key_val)
+
+        for c in range(self.num_columns):
+            self.index.add_entry(c, base_rid, values[4 + c])
+
+        return base_rid
+
+
 
     def update_tail_record(self, base_rid: int, update_cols: dict[int, int]):
-        pass
+        
+        if base_rid not in self.page_directory:
+            raise KeyError("record not found")
+        if base_rid in self.deleted:
+            raise KeyError("record has been deleted")
+
+        tail_rid = self.get_RID()
+        last_tail_rid = self.base_indirection.get(base_rid, INVALID_RID)
+
+        curr_vals = self.latest_cols(base_rid)
+        new_vals = curr_vals.copy()
+        for col_ids, val in update_cols.items():
+            new_vals[col_ids] = val
+        
+        curr_schema = self.base_schema.get(base_rid, 0)
+        new_schema = curr_schema
+        for col_ids in update_cols.keys():
+            new_schema |= (1 << col_ids)
+
+        curr_time = int(time())
+
+        values = [0] * self.total_columns
+        values[INDIRECTION_COLUMN] = last_tail_rid
+        values[RID_COLUMN] = tail_rid
+        values[TIMESTAMP_COLUMN] = curr_time
+        values[SCHEMA_ENCODING_COLUMN] = new_schema
+        values[4:] = new_vals
+
+        pages_id, index = self.write_record(is_tail = True, values = values)
+
+        self.base_indirection[base_rid] = tail_rid
+        self.base_schema[base_rid] = new_schema
+
+        for col_ids, val in update_cols.items():
+            old_val = curr_vals[col_ids]
+            self.index.update_entry(col_ids, base_rid, old_val, val)
+
+        return tail_rid
+        
 
     def read_record(self, base_rid: int, projected_cols: list[int]):
-        pass
+        
+        if base_rid not in self.page_directory:
+            return  None
+        if base_rid in self.deleted:
+            return None
+
+        rid = self.latest_rid(base_rid)
+
+        cols = [None] * self.num_columns
+        for c in range(self.num_columns):
+            if projected_cols[c] == 1: 
+                cols[c] = self.read_val(rid, 4 + c)
+
+        key_val = self.read_val(rid, 4 + self.key)
+
+        return Record(base_rid, key_val, cols)
+        
 
     def delete(self, base_rid: int):
         
