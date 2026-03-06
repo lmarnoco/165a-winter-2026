3b19c0b9d1082e1041ffc3d61f84d606d6a678b0
Lance Arnoco <lmarnoco@ucdavis.edu>
2026-03-05 20:54:32 -0800

    Changed Lock manager to No Wait ProtoCol

--- lstore/lock_manager.py
+++ lstore/lock_manager.py
@@ -1,5 +1,3 @@
-from lstore.table import Table, Record
-
 import threading
 from collections import deque
 import time
@@ -15,40 +13,30 @@ class LockManager():
 
     #Have Check if the RID actually exists before this not in this file
     def lock(self, transaction, rid, type):
-
-        event = threading.Event()
-
         with self.latch:
-        
             record = self.lock_table.get(rid)
 
             if record is None:
-                self.lock_table[rid] = {"mode": type, "transactions_held": {transaction}, "queue": deque()}
+                self.lock_table[rid] = {"mode": type, "transactions_held" : {transaction}}
                 transaction.held_locks.append((self, rid))
                 return True
 
+            can_grant = False
 
-            if record["mode"] == "S" and type == "S" and not any(t == "X" for _, t, _ in record["queue"]):
+            if type == "S" and record["mode"] == "S":
+                can_grant = True
+            elif not record["transactions_held"]:
+                can_grant = True
+            
+            if can_grant:
                 record["transactions_held"].add(transaction)
                 transaction.held_locks.append((self,rid))
                 return True
             
-
-            elif not record["transactions_held"] and type == "X":
-                record["mode"] = "X"
-                record["transactions_held"].add(transaction)
-                transaction.held_locks.append((self, rid))
-                return True
-
-            record["queue"].append((transaction, type, event))
-        event.wait()
-
-        with self.latch:
-            record = self.lock_table.get(rid)
-            if record and transaction in record["transactions_held"]:
-                return True
             else:
                 return False
+
+            
 
     def unlock(self, rid, transaction):
         with self.latch:
@@ -58,33 +46,13 @@ class LockManager():
                 return False
             
             record["transactions_held"].remove(transaction)
+
             if not record["transactions_held"]:
                 record["mode"] = None
+            
+            return True
 
-            temp = deque()
-
-            while record["queue"]:          
-                next_transaction, next_type, next_event = record["queue"].popleft()
-
-                if next_type == "S":
-                    if record["mode"] in ("S", None):
-                        record["mode"] = "S"
-                        record["transactions_held"].add(next_transaction)
-                        next_transaction.held_locks.append((self, rid))
-                        next_event.set()
-                    else:
-                        temp.append((next_transaction, next_type, next_event))
-
-                elif next_type == "X":
-                    if not record["transactions_held"]:
-                        record["mode"] = "X"
-                        record["transactions_held"].add(next_transaction)
-                        next_transaction.held_locks.append((self, rid))
-                        next_event.set()
-                    else:
-                        temp.append((next_transaction, next_type, next_event))
-            record["queue"] = temp
-        return True
+           
 
 
     
